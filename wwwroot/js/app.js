e29dffea7d646dd801f391a8a42653eda43df3dd
Matheus RCG <matheus.rcg.0@gmail.com>
2025-10-04 16:01:44 -0400

    style: Aprimora CSS e JavaScript da interface
    - Melhora estilos visuais no app.css
    - Atualiza funcionalidades JavaScript no app.js
    - Refinamentos na experiência do usuário

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,10 +1,10 @@
 window.initScrollToBottom = () => {
-    // Função para rolar para o final das mensagens
+  // Função para rolar para o final das mensagens
 };
 
 window.scrollToBottom = (elementId) => {
-    const element = document.getElementById(elementId);
-    if (element) {
-        element.scrollTop = element.scrollHeight;
-    }
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.scrollTop = element.scrollHeight;
+  }
 };
